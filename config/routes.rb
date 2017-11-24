97423827997553f1950d9e53af0a2bc82ba964a9
mateoc10 <mateoc10@gmail.com>
2017-11-24 13:14:35 +0000

    receive call fro shipment

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'shipments/new'
   
-  get '/shipments/progress', to: 'shipments#get_drivers_in_progress_shipments'
-  get '/shipments/delivered', to: 'shipments#get_drivers_delivered_shipments'
+  get '/in_progress', to: 'shipments#get_drivers_in_progress_shipments'
+  get '/delivered', to: 'shipments#get_drivers_delivered_shipments'
+  get '/details', to: 'shipments#details'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
