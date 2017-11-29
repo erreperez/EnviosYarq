9a72829dcf4937d211a98b9ad5b621e9fbf8b4bb
mateoc10 <mateoc10@gmail.com>
2017-11-29 19:35:05 +0000

    started creating shipment. request current user from users

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -35,19 +35,18 @@ class ShipmentsController < ApplicationController
   def create
     pp 'llegaranskinski'
     @Shipment = Shipment.new(user_params)
-    originLoc = create_location(params[:originLat], params[:originLng])
-    destinationLoc = create_location(params[:destinationLat], params[:destinationLng])
-    originLoc.save
-    destinationLoc.save
-    @Shipment.origin = originLoc
-    @Shipment.destination = destinationLoc
+    @Shipment.originLat = params[:originLat]
+    @Shipment.originLng = params[:originLng]
+    @Shipment.destinationLat = params[:destinationLat]
+    @Shipment.destinationLng = params[:destinationLng]
     @Shipment.state = 'In Progress'
     @Shipment.date = DateTime.now
-    @Shipment.sender = current_user
-    @Shipment.driver = Driver.find_by_id(params[:shipment][:driver])
+    @Shipment.sender = params[:current_user]
+    # @Shipment.driver = Driver.find_by_id(params[:shipment][:driver])
     @Shipment.price = params[:price_per_kilo] * @Shipment.weight
     
-    @user = User.find_by_id(current_user.id)
+    # @user = User.find_by_id(current_user.id)
+    @user = get_current_user(@Shipment.sender)
     
     if @user.discounts > 0
       @Shipment.price = @Shipment.price / 2
@@ -94,7 +93,6 @@ class ShipmentsController < ApplicationController
   def get_drivers_in_progress_shipments
     current_driver = params[:driver]
     @shipment_in_progress = Shipment.where(:state => 'In Progress' , :driver_id => current_driver) 
-    pp "ca;o", current_driver,  @shipment_in_progress
     render json: @shipment_in_progress
   end
   
@@ -119,6 +117,12 @@ class ShipmentsController < ApplicationController
   
 
   private
+  
+    def get_current_user(userId)
+      uri = 'get_user?userId=' + userId
+      resp = Faraday.get(ENV['URL_APP'] + uri)
+      pp 'current user', resp.body
+    end
     
     def user_params
       params.require(:shipment).permit(:weight, :payment)
