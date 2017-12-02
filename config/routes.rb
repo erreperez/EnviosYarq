d40db77dd6cf22e3aecfb43b26c4a1e816a7596a
Rodrigo <erreperez2@gmail.com>
2017-12-02 18:22:23 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   post '/create', to: 'shipments#create'
   get '/get_address', to: 'shipments#get_location_details'
   post '/end_shipment', to: 'shipments#end_shipment'
+  get '/find_by_id', to: 'shipments#find_shipment_by_id'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
